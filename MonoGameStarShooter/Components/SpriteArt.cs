0c45f622a8b0d401eedb5076d081f032bdd93b10
AlexJeter <Ajayjeter17@gmail.com>
2022-07-25 16:42:07 -0700

    Adding small changes and cleaning up some code

--- MonoGameStarShooter/Components/SpriteArt.cs
+++ MonoGameStarShooter/Components/SpriteArt.cs
@@ -6,12 +6,14 @@
         public static Texture2D Player { get; private set; }
         public static Texture2D EnemyTypeOne { get; private set; }
 
+        public static Texture2D EnemyTypeTwo { get; private set; }
 
         //Load the textures into the references from the Content mgcb editor
         public static void Load(ContentManager content)
         {
             Player = content.Load<Texture2D>("Sprites/PlayerShip");
             EnemyTypeOne = content.Load<Texture2D>("Sprites/EnemyStarship1Fix");
+            EnemyTypeTwo = content.Load<Texture2D>("Sprites/EnemyStarship2");
         }
 
         //Call the texture by using "SpriteArt.Name" ex "image = SpriteArt.Player" 

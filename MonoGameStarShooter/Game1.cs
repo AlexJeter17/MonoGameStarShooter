e8c48b986aef1a5ab3ebee5d18c75b876546d604
AlexJeter <Ajayjeter17@gmail.com>
2022-08-01 13:56:40 -0700

    Made a UI for the game so it doesn't start off execution

--- MonoGameStarShooter/Game1.cs
+++ MonoGameStarShooter/Game1.cs
@@ -6,6 +6,8 @@
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private SpriteFont font;
+        private bool inGame = false;
+
  
         public Game1()
         {
@@ -34,28 +36,60 @@
 
         protected override void Update(GameTime gameTime)
         {
-            EntityCollections.Initialize();
-            EntityCollections.Update();
-            WaveManager.Update();
-            //awave = WaveManager.wave;
-            base.Update(gameTime);
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter)) { 
+                inGame = true;
+            }
+            if (inGame)
+            {
+                EntityCollections.Initialize();
+                EntityCollections.Update();
+                WaveManager.Update();
+                //awave = WaveManager.wave;
+                base.Update(gameTime);
+
+            }
+            else {
+                _spriteBatch.Begin();
+
+                _spriteBatch.DrawString(font, "Press the ENTER button to start!", new Vector2(10, 10), Color.IndianRed);
+
+                _spriteBatch.End();
+
+            }
+
+
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            if (inGame)
+            {
+                GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(SpriteArt.backGround, new Rectangle(0, 0,GameManager.screenWidth,GameManager.screenHeight), Color.DarkGray); // If background does not perfectly fit screen, Edit it here
+                // TODO: Add your drawing code here
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(SpriteArt.backGround, new Rectangle(0, 0, GameManager.screenWidth, GameManager.screenHeight), Color.WhiteSmoke); // If background does not perfectly fit screen, Edit it here
 
-            EntityCollections.Draw(_spriteBatch);
-            _spriteBatch.DrawString(font, "Score: " + EntityCollections.score, new Vector2(10, 10), Color.White);
-            _spriteBatch.DrawString(font, "Wave: " + WaveManager.wave, new Vector2(GameManager.screenWidth - 200, 10), Color.White);
-            _spriteBatch.DrawString(font, "HP: " + EntityCollections.player1.hp, new Vector2(400, 10), Color.White);
-            _spriteBatch.End();
+                EntityCollections.Draw(_spriteBatch);
+                _spriteBatch.DrawString(font, "Score: " + EntityCollections.score, new Vector2(10, 10), Color.White);
+                _spriteBatch.DrawString(font, "Wave: " + WaveManager.wave, new Vector2(GameManager.screenWidth - 200, 10), Color.White);
+                _spriteBatch.DrawString(font, "HP: " + EntityCollections.player1.hp, new Vector2(400, 10), Color.White);
+                _spriteBatch.End();
 
-            base.Draw(gameTime);
+                base.Draw(gameTime);
+            }
+            else
+            {
+
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(SpriteArt.backGround, new Rectangle(0, 0, GameManager.screenWidth, GameManager.screenHeight), Color.WhiteSmoke);
+                _spriteBatch.DrawString(font, "Press ENTER to start the game!", new Vector2(GameManager.screenWidth / 10, GameManager.screenHeight / 2), Color.WhiteSmoke);
+                _spriteBatch.DrawString(font, "MonoGame Star Shooter Tutorial!", new Vector2(GameManager.screenWidth / 10, (GameManager.screenHeight / 2) - 100), Color.Red);
+                _spriteBatch.End();
+
+                base.Draw(gameTime);
+
+            }
         }
     }
 }
